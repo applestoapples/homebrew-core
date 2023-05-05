e680c43ae4a5e6092962776b836f3bbec93b5e58
ApplesToApples <svayn@yahoo.com>
2023-05-05 16:36:03 -0400

    Pin jmeter openjdk dependency to JDK17
    The current version of JMeter is tested only to JDK17: https://jmeter.apache.org/changes.html#New%20and%20Noteworthy
    One of its plugins, Groovy, does not support >JDK17 and throws a runtime exception when compiling scripts: https://groovy-lang.org/releasenotes/groovy-4.0.html#Groovy4.0-requirements

--- Formula/jmeter.rb
+++ Formula/jmeter.rb
@@ -10,7 +10,7 @@ class Jmeter < Formula
     sha256 cellar: :any_skip_relocation, all: "ad4c2d2fd4fe481f9443824e6555240d68b26bd01a7ce8444f755fe20f32e51e"
   end
 
-  depends_on "openjdk"
+  depends_on "openjdk@17"
 
   resource "jmeter-plugins-manager" do
     url "https://search.maven.org/remotecontent?filepath=kg/apc/jmeter-plugins-manager/1.7/jmeter-plugins-manager-1.7.jar"
@@ -22,7 +22,7 @@ class Jmeter < Formula
     rm_f Dir["bin/*.bat"]
     prefix.install_metafiles
     libexec.install Dir["*"]
-    (bin/"jmeter").write_env_script libexec/"bin/jmeter", JAVA_HOME: Formula["openjdk"].opt_prefix
+    (bin/"jmeter").write_env_script libexec/"bin/jmeter", JAVA_HOME: Formula["openjdk@17"].opt_prefix
 
     resource("jmeter-plugins-manager").stage do
       (libexec/"lib/ext").install Dir["*"]
